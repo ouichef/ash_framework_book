cec3fd5782648d389a7635124db8378d0a347ae1
Roman <roman@getaclooney.com>
2025-03-12 21:01:47 -0700

    adding resource actions

---
+++ .iex.exs
@@ -0,0 +1,4 @@
+alias Tunez.Music
+alias Tunez.Music.Artist
+alias Ash.Query, as: Q
+alias Ash.Changeset, as: C
--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -1,5 +1,42 @@
 defmodule Tunez.Music.Artist do
   use Ash.Resource,
     otp_app: :tunez,
-    domain: Tunez.Music
+    domain: Tunez.Music,
+    data_layer: AshPostgres.DataLayer
+
+  postgres do
+    table "artists"
+    repo Tunez.Repo
+  end
+
+  actions do
+    create :create do
+      accept [:name, :biography]
+    end
+
+    read :read do
+      primary? true
+    end
+
+    update :update do
+      accept [:name, :biography]
+    end
+
+    destroy :destroy do
+      
+    end
+  end
+
+  attributes do
+    uuid_primary_key :id
+
+    attribute :name, :string do
+      allow_nil? false
+    end
+
+    attribute :biography, :string
+
+    create_timestamp :inserted_at
+    create_timestamp :updated_at
+  end
 end
